e963b1d70d0c3e1f58485bedec96ef8c38509b4b
pazesilvajulia <pazesilvajulia@gmail.com>
2025-10-09 18:48:20 -0300

    inicio backend

--- Backend/app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../Frontend/HTMLCSS/index.html'));
-});
-
-module.exports = app;
